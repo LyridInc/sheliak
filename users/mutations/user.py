fa4f67e385e480afe6f8591685ffb108a98f2480
David Lu <dlu@lyrid.io>
2022-03-10 15:12:10 -0800

    Added DeleteMutation, changed ClassLogin FK to CASCADE

--- users/mutations/user.py
+++ users/mutations/user.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from graphql_auth.bases import MutationMixin
 from django.utils.translation import gettext as _
 from graphql_jwt.decorators import staff_member_required, login_required
-from graphene_django_cud.mutations import DjangoCreateMutation, DjangoPatchMutation
+from graphene_django_cud.mutations import DjangoCreateMutation, DjangoPatchMutation, DjangoDeleteMutation
 
 User = get_user_model()
 
@@ -71,3 +71,17 @@ class UserPatchAdmin(DjangoPatchMutation, MutationMixin):
     @staff_member_required
     def mutate(cls, root, info, input, id):
         return super().mutate(root, info, input, id)
+
+
+class DeleteUserAdmin(DjangoDeleteMutation, MutationMixin):
+    """
+    Delete User by admin form
+    """
+
+    class Meta:
+        model = User
+
+    @classmethod
+    @staff_member_required
+    def mutate(cls, root, info, id):
+        return super().mutate(root, info, id)
