93da4036bb33f64516881acaecb6ea4fd0ad7658
ewinata <ermanowinatawml@gmail.com>
2022-03-24 12:33:38 +0700

    fixed merge conflicts in variable names

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
